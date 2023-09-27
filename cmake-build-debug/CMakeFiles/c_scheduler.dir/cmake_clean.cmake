79a4ae20440c564ae08d50aa3228b7ee9bd88359
solaire <contact@unharmfulhazard.com>
2023-09-27 20:04:43 -0300

    Configured IDE settings

---
+++ cmake-build-debug/CMakeFiles/c_scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_scheduler.dir/scheduler.c.o"
+  "CMakeFiles/c_scheduler.dir/scheduler.c.o.d"
+  "c_scheduler"
+  "c_scheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/solaire/CLionProjects/c-scheculer
+# Install script for directory: /home/solaire/CLionProjects/c-scheduler
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/solaire/CLionProjects/c-scheculer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/solaire/CLionProjects/c-scheduler/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
